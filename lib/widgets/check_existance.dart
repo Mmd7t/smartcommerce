3a29314edcbee4dc2b89d7913c9968452edac9e7
Mmd7t <mmedhatmostafa3@gmail.com>
2021-05-06 15:26:41 -0700

    check_existing

--- lib/widgets/check_existance.dart
+++ lib/widgets/check_existance.dart
@@ -5,13 +5,14 @@ checkExisting() {
   final controller = Get.find<ProductsController>();
   if (controller.productDetails.value.inStock) {
     if (controller.productDetails.value.manageStock) {
-      if (controller.productDetails.value.qty != null) {
+      if (controller.productDetails.value.qty != null &&
+          controller.productDetails.value.qty != 0) {
         return true;
       } else {
         return false;
       }
     } else {
-      return false;
+      return true;
     }
   } else {
     return false;
